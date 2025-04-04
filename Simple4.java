a336c66696c4c0677485610b47593fa49291e131
sudhamurari <sudhamurari74@gmail.com>
2025-04-04 11:04:53 +0530

    SAve this

---
+++ Simple4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Simple4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = scanner.nextInt();
+        if(num>0){
+            System.err.println("The number is Positive");
+        }
+        else
+        System.out.println("The Number is Negative");
+
+    }
+    
+}
